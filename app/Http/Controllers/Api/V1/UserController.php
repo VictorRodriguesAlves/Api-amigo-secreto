18ddb5f32768d4bad44caf26850ad4506861d43f
Victor Alves <longovictor23@gmail.com>
2024-11-21 23:34:39 -0300

    Endpoint para criar usuario

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    public function createUser(CreateUserRequest $request): JsonResponse
+    {
+        $created = User::query()->create([
+            'name' => $request->validated('user_name'),
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully created user.',
+            'data' => $created
+        ], 201);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\V1\GroupController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1')->group(function () {
    Route::prefix('/group')->group(function () {
-       Route::post('/', [GroupController::class, 'createGroup']);
+       Route::post('/', [GroupController::class, 'createGroup'])->name('api.group.create');
    });
+
+   Route::post('/user', [UserController::class, 'createUser'])->name('api.user.create');
+
 });
 
